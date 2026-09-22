b16a3851fb2951a5e172c76f4ea7745e38419120
Suxo <51209091+zurr17@users.noreply.github.com>
2026-09-22 15:43:55 +0700

    Update BukuDigital.java

--- src/BukuDigital.java
+++ src/BukuDigital.java
@@ -1,6 +1,6 @@
 public class BukuDigital extends Buku {
-    double ukuranFile; // ini dalam MB
-    String formatFile; // contohnya kek: PDF, EPUB
+    double ukuranFile; 
+    String formatFile; 
 
     public BukuDigital(int id, String judul, String author, double ukuranFile, String formatFile) {
         super(id, judul, author);
@@ -8,14 +8,17 @@ public class BukuDigital extends Buku {
         this.formatFile = formatFile;
     }
 
-    // method spesifik child class
     public void unduhBuku() {
         System.out.println("Mengunduh file buku " + judul + " (" + formatFile + ", " + ukuranFile + " MB)...");
     }
 
-    // bisa override method tambahBuku biar lebih kelihatan bedanya
     @Override
     public void tambahBuku() {
         System.out.println("Buku digital " + judul + " (" + formatFile + ") berhasil ditambahkan.");
+    }
+
+    @Override
+    public void infoBuku() {
+        System.out.println("[E-Book] " + judul + " oleh " + author + " (" + formatFile + ", " + ukuranFile + "MB)");
     }
 }
